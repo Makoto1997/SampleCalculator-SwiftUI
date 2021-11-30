4cc0ac545b4911b4acba1d66229b362f96e8ab88
Makoto1997 <mkt1997gg@gmail.com>
2021-11-30 22:15:43 +0900

    InitialCommit

---
+++ SampleCalculator/SampleCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleCalculatorApp.swift
+//  SampleCalculator
+//
+//  Created by Makoto on 2021/11/30.
+//
+
+import SwiftUI
+
+@main
+struct SampleCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
